42786ba01530bbfe54fc5909c376286116823abc
MrCronkite <vladshima99@gmail.com>
2023-07-03 23:08:13 +0300

    testing bags

--- menuTestTaskTests/menuTestTaskTests.swift
+++ menuTestTaskTests/menuTestTaskTests.swift
@@ -8,22 +8,19 @@
 import XCTest
 
 final class menuTestTaskTests: XCTestCase {
-
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
     }
-
+    
     func testExample() throws {
     }
-
+    
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
         measure {
-            // Put the code you want to measure the time of here.
+            
         }
     }
-
 }
